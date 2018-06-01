084d6b21458dd562c2d0923402422b19cd182d60
T-101 <T-101@PC220>
2018-05-31 19:41:40 -0500

    cons2

---
+++ objetos-1803/src/constructores2/ProbarConstructores.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructores2;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author T-101
+ */
+public class ProbarConstructores {
+    public static void main(String[] args) {
+        
+        Persona p1=new Persona("ARMANDO");
+        Persona p2=new Persona();
+        JFrame ventanita=new JFrame("HOLA MUNDO");
+        ventanita.setSize(300,300);
+        ventanita.setVisible(true);
+    }
+}
